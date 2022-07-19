b61bb9bab63b6538fb21923833dbd5eba255989e
ryudai <bigdragon.06102001@gmail.com>
2022-07-19 17:19:58 +0900

    firebaseと接続

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
+import { collection, doc, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase/firebase-config";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [todos, setTodos] = useState([]);
+  const todosCollectionRef = collection(db, "todos");
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const data = await getDocs(todosCollectionRef);
+      setTodos(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getTodos();
+  }, []);
+
+  return (
+    <>
+      {todos.map((todo) => {
+        return (
+          <ul>
+            <li>{todo.title}</li>
+            <li>{todo.detail}</li>
+          </ul>
+        );
+      })}
+    </>
+  );
 }
