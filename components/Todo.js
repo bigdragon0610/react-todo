24d43c80118a00cfeaacb0069621bd5ba09c5833
ryudai <bigdragon.06102001@gmail.com>
2022-07-22 02:26:36 +0900

    削除機能追加

--- components/Todo.js
+++ components/Todo.js
@@ -1,8 +1,27 @@
-const Todo = ({ props }) => {
+import { deleteDoc, doc } from "firebase/firestore";
+import { MdOutlineDeleteOutline } from "react-icons/md";
+
+const Todo = ({ todo, todosCollectionRef, setTodos }) => {
+  const deleteTodo = () => {
+    if (window.confirm("Delete?")) {
+      deleteDoc(doc(todosCollectionRef, todo.id));
+      setTodos((prevTodos) => {
+        return prevTodos.filter((prevTodo) => prevTodo.id !== todo.id);
+      });
+    }
+  };
+
   return (
     <section className="w-60 h-60 bg-blue-50 px-3 pt-3 shadow-md">
-      <h2 className="font-bold text-xl mb-2">{props.title}</h2>
-      <p className="">{props.detail}</p>
+      <div className="h-3/4">
+        <h2 className="font-bold text-xl mb-2">{todo.title}</h2>
+        <p>{todo.detail}</p>
+      </div>
+      <div className="h-1/4 w-full flex">
+        <button className="cursor-pointer ml-auto my-auto" onClick={deleteTodo}>
+          <MdOutlineDeleteOutline className="text-2xl" />
+        </button>
+      </div>
     </section>
   );
 };
